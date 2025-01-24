5abde4659d517462da7f38ca9b8f3db1663dfb87
Abdel Younes <abdel.younes@synaptics.com>
2025-01-24 16:51:59 +0100

    fix: use env variable for toolchain dir

--- cmake/arm-gnu-toolchain-aarch64.cmake
+++ cmake/arm-gnu-toolchain-aarch64.cmake
@@ -3,13 +3,15 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
-set(TOOLCHAIN_PREFIX "aarch64-none-linux-gnu-")
+set(TOOLCHAIN_DIR $ENV{TOOLCHAIN_DIR})
+set(TOOLCHAIN_PREFIX aarch64-none-linux-gnu-)
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/${TOOLCHAIN_PREFIX}g++)
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pie -Wl,--no-undefined -Wl,--no-allow-shlib-undefined")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++")
 
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
